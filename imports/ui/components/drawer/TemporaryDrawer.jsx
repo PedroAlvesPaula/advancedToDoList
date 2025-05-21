aa9976224d2979098391989c9d1c071f09c220ce
Pedro Alves <pedropaula2325p@gmail.com>
2025-05-21 14:08:17 -0300

    refact: troca a forma de como recuperar os dados do usuario

--- imports/ui/components/drawer/TemporaryDrawer.jsx
+++ imports/ui/components/drawer/TemporaryDrawer.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { useTracker } from 'meteor/react-meteor-data';
 
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
@@ -15,71 +14,89 @@ import MailIcon from '@mui/icons-material/Mail';
 import Avatar from '@mui/material/Avatar';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const TemporaryDrawer = ({toggleDrawer, open, buttonsDrawer }) => {
 
-  const user = useTracker(() => Meteor.user());
+  const [user, setUser] = useState([]);
+  const [profileImage, setProfileImage] = useState(null);
 
-  const {username, emails, profile} = user;
-  const userInformations = [username, emails[0].address];
+  useEffect(() => {
+    Meteor.call('users.getUserInformations', (error, result) => {
+      if(error) {
+        console.error('Erro ao buscar as informações do usuário', error);
+      } else {
+        setUser([ result.username, 
+                  result.email, 
+                ]);
+        setProfileImage(result.profileImage)
+      }
+    });
+  }, []);
 
   const navigate = useNavigate();
 
-  const navigateToUserProfile = () => {
-    navigate('/userProfile');
-  }
-
   return (
     <div>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        <Box sx={{ width: 320, 
-          backgroundColor: '#8c77fe', 
-          height: '100vh', 
-          color: '#E0E2E6',
-          }} 
-          role="presentation" 
-          onClick={toggleDrawer(false)}
-        >
-          <List>
-            <ListItem sx={{alignItems: 'center', justifyContent: 'center'}}>
-              <Avatar
-                alt='Imagem de perfil'
-                src={profile.profileImage}
-                sx={{ width: 128, height: 128 }}
-              />
-            </ListItem>
-            {userInformations.map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemIcon sx={{padding: '8px 18px'}}>
-                  {index === 0 ? <AccountCircleIcon sx={{color: '#E0E2E6'}} fontSize='large'/> : <MailIcon sx={{color: '#E0E2E6'}} fontSize='large'/>}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+      {user && 
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <Box sx={{ width: 320, 
+            backgroundColor: '#8c77fe', 
+            height: '100vh', 
+            color: '#E0E2E6',
+            }} 
+            role="presentation" 
+            onClick={toggleDrawer(false)}
+          >
+            <List>
+              <ListItem sx={{alignItems: 'center', justifyContent: 'center'}}>
+                <Avatar
+                  alt='Imagem de perfil'
+                  src={profileImage}
+                  sx={{ width: 128, height: 128 }}
+                />
               </ListItem>
-            ))}
-            <ListItem sx={{width: '100%', padding: 0}}>
-                <ListItemButton onClick={navigateToUserProfile}>
-                  <ListItemIcon>
-                    <ManageAccountsIcon sx={{color: '#E0E2E6'}} fontSize='large'/>
+              {user.map((text, index) => (
+                <ListItem key={index} disablePadding>
+                  <ListItemIcon sx={{padding: '8px 18px'}}>
+                    {index === 0 ? <AccountCircleIcon sx={{color: '#E0E2E6'}} fontSize='large'/> : <MailIcon sx={{color: '#E0E2E6'}} fontSize='large'/>}
                   </ListItemIcon>
-                    <ListItemText primary='Acessar perfil'/>
-                </ListItemButton>
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            {buttonsDrawer.map((action, index) => (
-              <ListItem key={index} disablePadding>
-                <ListItemButton onClick={action.click}>
-                  <ListItemIcon>
-                    {action.icon}
-                  </ListItemIcon>
-                  <ListItemText primary={action.text} />
-                </ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+              <ListItem sx={{width: '100%', padding: 0}}>
+                  <ListItemButton onClick={() => navigate('/userProfile')}>
+                    <ListItemIcon>
+                      <ManageAccountsIcon sx={{color: '#E0E2E6'}} fontSize='large'/>
+                    </ListItemIcon>
+                      <ListItemText primary='Acessar perfil'/>
+                  </ListItemButton>
               </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
+            </List>
+            <Divider />
+            <List>
+              {buttonsDrawer.map((action, index) => (
+                <ListItem key={index} disablePadding>
+                  <ListItemButton onClick={action.click}>
+                    <ListItemIcon>
+                      {action.icon}
+                    </ListItemIcon>
+                    <ListItemText primary={action.text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+              <ListItem sx={{width: '100%', padding: 0}}>
+                  <ListItemButton onClick={() => {Meteor.logout; navigate('/');}}>
+                    <ListItemIcon>
+                      <LogoutIcon sx={{color: '#E0E2E6'}} fontSize='large'/>
+                    </ListItemIcon>
+                      <ListItemText primary='Sair'/>
+                  </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+      }
     </div>
   );
 }
