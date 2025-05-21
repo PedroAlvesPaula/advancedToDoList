a6f893e75f4b7ca06ede0f9d90de4777289c7c0f
Pedro Alves <pedropaula2325p@gmail.com>
2025-05-21 14:24:45 -0300

    feat: adiciona acao na tela de boas vindas para acessar todas as tarefas

--- imports/ui/components/welcome/CardsWelcome.jsx
+++ imports/ui/components/welcome/CardsWelcome.jsx
@@ -4,6 +4,8 @@ import { Meteor } from 'meteor/meteor';
 import { Grid, Card, CardContent, Typography } from "@mui/material";
 import { TasksCollection } from '../../../api/tasksCollection';
 import { useTracker } from 'meteor/react-meteor-data';
+import IconButton from '@mui/material/IconButton';
+import AssignmentIcon from '@mui/icons-material/Assignment';
 
 export const CardsWelcome = () => {
     const user = useTracker(() => Meteor.user());
@@ -48,9 +50,38 @@ export const CardsWelcome = () => {
             }}
         >
             {data.map((item, index) => (
+                <Grid 
+                    size={{xs:12, md: 6}} 
+                    key={index}
+                >
+                    <Card sx={{ 
+                            width: '100%',
+                            minHeight: 150,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            flexDirection: 'column',
+                            backgroundColor: '#8c77fe',
+                        }}>
+                        <CardContent sx={{ 
+                                            textAlign: 'center', 
+                                            width: '100%',
+                                        }}>
+                            <Typography variant="h6" gutterBottom sx={{color: '#E0E2E6', letterSpacing: '2px'}}>
+                                {item.title}
+                            </Typography>
+                            <Typography variant="h4" color="primary" gutterBottom sx={{color: '#120045'}}>
+                                {item.number}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" sx={{color: '#E0E2E6', letterSpacing: '2px'}}>
+                                {item.description}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
             <Grid 
                 size={{xs:12, md: 6}} 
-                key={index}
             >
                 <Card sx={{ 
                         width: '100%',
@@ -66,18 +97,19 @@ export const CardsWelcome = () => {
                                         width: '100%',
                                     }}>
                         <Typography variant="h6" gutterBottom sx={{color: '#E0E2E6', letterSpacing: '2px'}}>
-                        {item.title}
+                            Listar todas tarefas
                         </Typography>
                         <Typography variant="h4" color="primary" gutterBottom sx={{color: '#120045'}}>
-                        {item.number}
+                            <IconButton>
+                                <AssignmentIcon />
+                            </IconButton>
                         </Typography>
                         <Typography variant="body2" color="text.secondary" sx={{color: '#E0E2E6', letterSpacing: '2px'}}>
-                        {item.description}
+                            clique para lsitar todas as tarefas
                         </Typography>
                     </CardContent>
                 </Card>
             </Grid>
-            ))}
         </Grid>
     </>
   )
